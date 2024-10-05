2e2eebfaaeb77edca4a9374c463d8663d2482afc
woogym <kwj9294@gmail.com>
2024-10-06 05:20:54 +0900

    feat[#2]: JwtAuthenticationProcessingFilter

---
+++ LoginHub/src/main/java/com/woojin/loginhub/jwt/JwtAuthenticationProcessingFilter.java
@@ -0,0 +1,108 @@
+package com.woojin.loginhub.jwt;
+
+import com.woojin.loginhub.domain.User;
+import com.woojin.loginhub.global.util.PasswordUtil;
+import com.woojin.loginhub.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * Jwt 인증 필터
+ * "/login" 이외의 URI 요청이 왔을 때 처리하는 필터
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
+
+    private static final String NO_CHECK_URL = "/login"; // "/login"으로 들어오는 요청은 Filter 작동 X
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    private final GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
+            return;
+        }
+
+        // RefreshToken 추출 및 유효성 검사
+        String refreshToken = tokenService.extractRefreshToken(request)
+                .filter(tokenService::validateToken)
+                .orElse(null);
+
+        if (refreshToken != null) {
+            // RefreshToken이 유효하다면, AccessToken과 RefreshToken 재발급
+            checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
+            return;
+        }
+
+        // AccessToken을 검사하고 인증을 처리하는 로직
+        checkAccessTokenAndAuthentication(request, response, filterChain);
+    }
+
+    /**
+     *  [리프레시 토큰으로 유저 정보 찾기 & 액세스 토큰/리프레시 토큰 재발급 메소드]
+     */
+    public void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
+        tokenService.extractEmail(refreshToken)
+                .flatMap(userRepository::findByEmail)
+                .ifPresent(user -> {
+                            String newAccessToken = tokenService.createAccessToken(user);
+                            String newRefreshToken = tokenService.createRefreshToken();
+                            user.updateRefreshToken(newRefreshToken);  // 유저의 리프레시 토큰을 업데이트
+                            tokenService.sendAccessAndRefreshToken(response, newAccessToken, newRefreshToken);
+        });
+    }
+
+    /**
+     * [액세스 토큰 체크 & 인증 처리 메소드]
+     */
+    public void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("checkAccessTokenAndAuthentication() 호출");
+        tokenService.extractAccessToken(request)
+                .filter(tokenService::validateToken)
+                .flatMap(tokenService::extractEmail)
+                .flatMap(userRepository::findByEmail)
+                .ifPresent(this::saveAuthentication);
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * [인증 허가 메소드]
+     */
+    public void saveAuthentication(User user) {
+        String password = user.getPassword();
+        if (password == null) { // 소셜 로그인 유저의 비밀번호 임의로 설정 하여 소셜 로그인 유저도 인증 되도록 설정
+            password = PasswordUtil.generateRandomPassword();
+        }
+
+        UserDetails userDetailsUser = org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(password)
+                .roles(user.getRole().name())
+                .build();
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(userDetailsUser, null,
+                        authoritiesMapper.mapAuthorities(userDetailsUser.getAuthorities()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
