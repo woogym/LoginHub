dfd90258d39b4812a564845637829ea47030a7de
woogym <kwj9294@gmail.com>
2024-10-05 23:37:43 +0900

    initailize commit

---
+++ LoginHub/src/main/java/com/woojin/loginhub/LoginHubApplication.java
@@ -0,0 +1,13 @@
+package com.woojin.loginhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginHubApplication.class, args);
+    }
+
+}
---
+++ LoginHub/src/test/java/com/woojin/loginhub/LoginHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojin.loginhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
