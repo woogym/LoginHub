759ab34f731f19ec6f04a4b0e35f9b13f250c374
woogym <kwj9294@gmail.com>
2024-10-16 04:57:56 +0900

    feat[#3]: implement GoogleOauth2UserInfo class

---
+++ LoginHub/src/main/java/com/woojin/loginhub/oauth2/userinfo/GoogleOauth2UserInfo.java
@@ -0,0 +1,25 @@
+package com.woojin.loginhub.oauth2.userinfo;
+
+import java.util.Map;
+
+public class GoogleOauth2UserInfo extends Oauth2UserInfo {
+
+    public GoogleOauth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getNickname() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) attributes.get("picture");
+    }
+}
