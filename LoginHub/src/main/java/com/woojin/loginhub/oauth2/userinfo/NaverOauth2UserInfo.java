9f92ffe67c3cd566777787a1bb11936a0dfc06ad
woogym <kwj9294@gmail.com>
2024-10-17 04:58:27 +0900

    feat[#3]: implement KakaoOauth2UserInfo class

---
+++ LoginHub/src/main/java/com/woojin/loginhub/oauth2/userinfo/NaverOauth2UserInfo.java
@@ -0,0 +1,43 @@
+package com.woojin.loginhub.oauth2.userinfo;
+
+import java.util.Map;
+
+public class NaverOauth2UserInfo extends Oauth2UserInfo{
+
+    public NaverOauth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        if (response == null) {
+            return null;
+        }
+
+        return (String) response.get("id");
+    }
+
+    @Override
+    public String getNickname() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        if (response == null) {
+            return null;
+        }
+
+        return (String) response.get("nickname");
+    }
+
+    @Override
+    public String getImageUrl() {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        if (response == null) {
+            return null;
+        }
+
+        return (String) response.get("profile_image");
+    }
+}
